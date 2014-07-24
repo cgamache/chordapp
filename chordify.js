da96b1d44acf335c6abfa27c278a37002d3c1f51
Anton Emery <antonemery@gmail.com>
2014-07-24 11:40:29 -0700

    drew guitar strings

--- chordify.js
+++ chordify.js
@@ -4,5 +4,10 @@ window.onload = function() {  //runs once window had loaded
 	var canvas = document.getElementById('paper'),
 	c = canvas.getContext('2d');  //specifies a 2d getContext
 	c.fillStyle = 'black';
-	c.fillRect(0, 0, canvas.width, canvas.height);
+	c.fillRect(20, 10, 3, 200);
+	c.fillRect(40, 10, 3, 200);
+	c.fillRect(60, 10, 3, 200);
+	c.fillRect(80, 10, 3, 200);
+	c.fillRect(100, 10, 3, 200);
+	c.fillRect(120, 10, 3, 200);
 };
