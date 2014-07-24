d7b59d465509838fee3c6722cfe1c375b1fbc93f
Anton Emery <antonemery@gmail.com>
2014-07-24 14:03:29 -0700

    auto draws chords based on starting coordinate

--- chordify.js
+++ chordify.js
@@ -7,6 +7,8 @@
 window.onload = function() {  //runs once window had loaded
 		var testchord = new chord(0, 0);
 		testchord.draw();
+		var testchord2 = new chord(100, 100);
+		testchord2.draw();
 	};
 
  //chord object
@@ -18,22 +20,24 @@ function chord (x, y) {
  	this.draw = function () {
 	 	c.fillStyle = 'black';
 	 		//draws strings
-		c.fillRect(20, 10, 3, 200);
-		c.fillRect(40, 10, 3, 200);
-		c.fillRect(60, 10, 3, 200);
-		c.fillRect(80, 10, 3, 200);
-		c.fillRect(100, 10, 3, 200);
-		c.fillRect(120, 10, 3, 200);
+		
+
+		c.fillRect(x, y, 3, 200);  //x, y, width, height
+		c.fillRect(x + 20, y, 3, 200);
+		c.fillRect(x + 40, y, 3, 200);
+		c.fillRect(x + 60, y, 3, 200);
+		c.fillRect(x + 80, y, 3, 200);
+		c.fillRect(x + 100, y, 3, 200);
 
 		//draws nut
-		c.fillRect(20, 10, 100, 5);
+		c.fillRect(x, y, 100, 5);
 
 		//frets
-		c.fillRect(20, 30, 100, 3);
-		c.fillRect(20, 50, 100, 3);
-		c.fillRect(20, 70, 100, 3);
-		c.fillRect(20, 90, 100, 3);
-		c.fillRect(20, 110, 100, 3);
+		c.fillRect(x, y, 100, 3);
+		c.fillRect(x, y + 20, 100, 3);
+		c.fillRect(x, y + 40, 100, 3);
+		c.fillRect(x, y + 60, 100, 3);
+		c.fillRect(x, y + 80, 100, 3);
 	 };
 
 
