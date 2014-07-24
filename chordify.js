4a2a3925695f26ae3f84caedd7bf8576e316e50b
Anton Emery <antonemery@gmail.com>
2014-07-24 11:08:36 -0700

    created black rectangular fill section

--- chordify.js
+++ chordify.js
@@ -2,5 +2,7 @@
 
 window.onload = function() {  //runs once window had loaded
 	var canvas = document.getElementById('paper'),
-	c = canvas.getContext('2d')
+	c = canvas.getContext('2d');  //specifies a 2d getContext
+	c.fillStyle = 'black';
+	c.fillRect(0, 0, canvas.width, canvas.height);
 };
